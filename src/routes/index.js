4d970347bbf678c220912c747d4e995278fe1be7
JuanesEspinosa <juanes.espinosa01@gmail.com>
2025-10-08 08:53:35 -0500

    ajuste

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,8 +7,11 @@ const authRoutes = require('./auth');
 const userRoutes = require('./users');
 const productRoutes = require('./products');
 const customerRoutes = require('./customer');
+const licorType = require('./tplicor')
+
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/products', productRoutes);
 router.use('/customers', customerRoutes);
+router.use('/licor-type', licorType);
 module.exports = router;
